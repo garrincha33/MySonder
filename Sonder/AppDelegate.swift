0fdff59ff30aa8b60613ac798198a07711dbc03b
Richard Price <mrspondooli@gmail.com>
2018-07-26 18:35:54 +0100

    added firebase

--- Sonder/AppDelegate.swift
+++ Sonder/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -19,10 +20,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         window = UIWindow()
         window?.makeKeyAndVisible()
-        window?.rootViewController = ViewController()
+        window?.rootViewController = MainFeedViewController()
         
         
-        
+        FirebaseApp.configure()
         return true
     }
 
---
+++ Sonder/MainFeedViewController.swift
@@ -0,0 +1,27 @@
+//
+//  MainFeedViewController.swift
+//  Sonder
+//
+//  Created by Richard Price on 26/07/2018.
+//  Copyright © 2018 twisted echo. All rights reserved.
+//
+
+import UIKit
+
+class MainFeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+     
+        view.backgroundColor = .red
+        
+        
+        
+        
+        
+    }
+
+ 
+
+}
+
