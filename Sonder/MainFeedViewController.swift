2a7e6147c95beb1478ef5710be1ffaa8430ddd16
Richard Price <mrspondooli@gmail.com>
2018-07-27 06:43:16 +0100

    added bg image

--- Sonder/MainFeedViewController.swift
+++ Sonder/MainFeedViewController.swift
@@ -16,21 +16,39 @@ class MainFeedViewController: UIViewController {
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
+    
+    let userNameTextField: UITextField = {
+        let tf = UITextField()
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        tf.textColor = .white
+        tf.attributedPlaceholder = NSAttributedString(string: "Username",
+        attributes: [NSAttributedStringKey.foregroundColor: UIColor(white: 1.0, alpha: 0.6)])
+        let bottomLayerUser = CALayer()
+        bottomLayerUser.frame = CGRect(x: 0, y: 29, width: 300, height: 0.6)
+        bottomLayerUser.backgroundColor = UIColor(red: 220/255, green: 220/255, blue: 175/255, alpha: 1).cgColor
+        tf.layer.addSublayer(bottomLayerUser)
+        return tf
+    }()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .black
+        view.backgroundColor = UIColor(patternImage: #imageLiteral(resourceName: "bg1"))
+        
         
         view.addSubview(addPhotoButton)
         addPhotoButton.heightAnchor.constraint(equalToConstant: 140).isActive = true
         addPhotoButton.widthAnchor.constraint(equalToConstant: 140).isActive = true
         addPhotoButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         addPhotoButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 40).isActive = true
+        
+        view.addSubview(userNameTextField)
+        userNameTextField.topAnchor.constraint(equalTo: addPhotoButton.bottomAnchor).isActive = true
+        userNameTextField.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        userNameTextField.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 40).isActive = true
+        userNameTextField.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -40).isActive = true
 
     }
-
- 
-
 }
 
