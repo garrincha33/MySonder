e6252e1b4324d42cf8266d4350a3de1c296adbfd
Richard Price <mrspondooli@gmail.com>
2018-07-31 18:40:14 +0100

    fetching user picture and adding to profile, fetching username and displaying in title

--- Sonder/UserProfileController.swift
+++ Sonder/UserProfileController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import FirebaseAuth
+import FirebaseDatabase
 import Firebase
 
 class UserProfileController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
@@ -14,17 +16,17 @@ class UserProfileController: UICollectionViewController, UICollectionViewDelegat
         super.viewDidLoad()
         
         collectionView?.backgroundColor = .white
-        
-        //navigationItem.title = Auth.auth().currentUser?.uid
-        //fetchUser()
-        
+        navigationItem.title = Auth.auth().currentUser?.uid
+        fetchUser()
         collectionView?.register(UserProfileHeader.self, forSupplementaryViewOfKind: UICollectionElementKindSectionHeader, withReuseIdentifier: "headerId")
 
     }
     
     override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
-        let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "headerId", for: indexPath)
-        //header.backgroundColor = .green
+        let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "headerId", for: indexPath) as! UserProfileHeader
+        
+        header.user = self.user
+    
         return header
     }
     
@@ -32,24 +34,24 @@ class UserProfileController: UICollectionViewController, UICollectionViewDelegat
         return CGSize(width: view.frame.width, height: 200)
     }
     
-//    fileprivate func fetchUser() {
-//
-//        guard let uid = Auth.auth().currentUser?.uid else { return }
-//        Database.database().reference().child("users").child(uid).observeSingleEvent(of: .value, with: { (snapshot) in
-//            print(snapshot.value ?? "")
-//
-//            guard let dictionary = snapshot.value as? [String: Any] else { return }
-//
-//            self.user = User(dictionary: dictionary)
-//            self.navigationItem.title = self.user?.username
-//
-//            self.collectionView?.reloadData()
-//
-//        }) { (err) in
-//            print("Failed to fetch user:", err)
-//        }
-//
-//    }
+    var user: User?
+    
+    fileprivate func fetchUser() {
+
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        Database.database().reference().child("users").child(uid).observeSingleEvent(of: .value, with: { (snapshot) in
+            print(snapshot.value ?? "")
+
+            guard let dictionary = snapshot.value as? [String: Any] else { return }
+            self.user = User(dictionary: dictionary)
+            self.navigationItem.title = self.user?.username
+            self.collectionView?.reloadData()
+
+        }) { (err) in
+            print("Failed to fetch user:", err)
+        }
+
+    }
 }
 
 struct User {
@@ -58,6 +60,6 @@ struct User {
     
     init(dictionary: [String: Any]) {
         self.username = dictionary["username"] as? String ?? ""
-        self.profileImageUrl = dictionary["profileImageUrl"]  as? String ?? ""
+        self.profileImageUrl = dictionary["profilePicUrl"]  as? String ?? ""
     }
 }
--- Sonder/UserProfileHeader.swift
+++ Sonder/UserProfileHeader.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseDatabase
+import FirebaseAuth
 
 class UserProfileHeader: UICollectionViewCell {
     
@@ -23,11 +26,36 @@ class UserProfileHeader: UICollectionViewCell {
         backgroundColor = .blue
         addSubview(profileImageView)
         profileImageView.anchor(top: topAnchor, left: self.leftAnchor, bottom: nil, right: nil, paddingTop: 12, paddingLeft: 12, paddingBottom: 0, paddingRight: 0, width: 80, height: 80)
-
+        profileImageView.layer.cornerRadius = 80 / 2
+        profileImageView.clipsToBounds = true
+        
+    }
+    
+    var user: User? {
+        didSet {
+            setupProfileImage()
+        }
+    }
+    
+    fileprivate func setupProfileImage() {
+        print("did set \(user?.username ?? "")")
+        guard let profileImageUrl = user?.profileImageUrl else {return}
+        guard let url = URL(string: profileImageUrl) else {return}
+        URLSession.shared.dataTask(with: url) { (data, response, err) in
+            if let err = err {
+                print("failed to fetch image", err)
+                return
+            }
+            guard let data = data else {return}
+            let image = UIImage(data: data)
+            DispatchQueue.main.async {
+                self.profileImageView.image = image
+                }
+            }.resume()
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
+    
 }
