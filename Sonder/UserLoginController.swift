ca9544c1de916e28d169eb560de91491a1f76edf
Richard Price <mrspondooli@gmail.com>
2018-08-04 11:25:12 +0100

    added text fields, login button and sonder logo

--- Sonder/UserLoginController.swift
+++ Sonder/UserLoginController.swift
@@ -10,10 +10,67 @@ import UIKit
 
 class UserLoginController: UIViewController {
     
+    let loginContainerView: UIView = {
+        
+        let view = UIView()
+        let logoImageView = UIImageView(image: #imageLiteral(resourceName: "bgsonder"))
+        view.addSubview(logoImageView)
+        logoImageView.anchor(top: view.topAnchor, left: nil, bottom: nil, right: nil, paddingTop: 80, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 300, height: 100)
+        logoImageView.contentMode = .scaleAspectFill
+        logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        logoImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        //view.backgroundColor = UIColor.rgb(red: 0, green: 120, blue: 175)
+        return view
+        
+        
+    }()
+    
     let signUpButton: UIButton = {
         let button = UIButton(type: .system)
         button.setTitle("Don't have an account?  Sign Up.", for: .normal)
         button.addTarget(self, action: #selector(handleShowSignUp), for: .touchUpInside)
+        return button
+    }()
+    
+    let emailTextField: UITextField = {
+        let tf = UITextField()
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        tf.textColor = .white
+        tf.attributedPlaceholder = NSAttributedString(string: "Email",
+            attributes: [NSAttributedStringKey.foregroundColor: UIColor(white: 1.0, alpha: 0.6)])
+        let bottomLayerUser = CALayer()
+        bottomLayerUser.frame = CGRect(x: 0, y: 29, width: 300, height: 0.6)
+        bottomLayerUser.backgroundColor = UIColor(red: 220/255, green: 220/255, blue: 175/255, alpha: 1).cgColor
+        tf.layer.addSublayer(bottomLayerUser)
+        //tf.addTarget(self, action: #selector(handleTextInputChange), for: .editingChanged)
+        return tf
+    }()
+    
+    let passwordTextField: UITextField = {
+        let tf = UITextField()
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        tf.textColor = .white
+        tf.isSecureTextEntry = true
+        tf.attributedPlaceholder = NSAttributedString(string: "Password",
+            attributes: [NSAttributedStringKey.foregroundColor: UIColor(white: 1.0, alpha: 0.6)])
+        let bottomLayerUser = CALayer()
+        bottomLayerUser.frame = CGRect(x: 0, y: 29, width: 300, height: 0.6)
+        bottomLayerUser.backgroundColor = UIColor(red: 220/255, green: 220/255, blue: 175/255, alpha: 1).cgColor
+        tf.layer.addSublayer(bottomLayerUser)
+       // tf.addTarget(self, action: #selector(handleTextInputChange), for: .editingChanged)
+        return tf
+    }()
+    
+    let loginButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Login", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.backgroundColor = UIColor.rgb(red: 149, green: 204, blue: 244)
+        button.layer.cornerRadius = 5
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        button.setTitleColor(.white, for: .normal)
+        //button.addTarget(self, action: #selector(handleSignUp), for: .touchUpInside)
+        button.isEnabled = false
         return button
     }()
     
@@ -22,15 +79,35 @@ class UserLoginController: UIViewController {
         navigationController?.pushViewController(signUpController, animated: true)
     }
     
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        view.addSubview(loginContainerView)
+        loginContainerView.anchor(top: view.topAnchor, left: view.leftAnchor, bottom: nil, right: view.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 150)
         navigationController?.isNavigationBarHidden = true
-        
-        view.backgroundColor = .white
-        
+        view.backgroundColor = UIColor(patternImage: #imageLiteral(resourceName: "bg1"))
         view.addSubview(signUpButton)
         signUpButton.anchor(top: nil, left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 50)
+        
+        setupInputFields()
+        
+        
+        
+    }
+    
+    fileprivate func setupInputFields() {
+        
+        let stackView = UIStackView(arrangedSubviews: [emailTextField, passwordTextField, loginButton])
+        stackView.axis = .vertical
+        stackView.spacing = 20
+        stackView.distribution = .fillEqually
+        
+        view.addSubview(stackView)
+        stackView.anchor(top: loginContainerView.bottomAnchor, left: view.leftAnchor, bottom: nil, right: view.rightAnchor, paddingTop: 40, paddingLeft: 30, paddingBottom: 0, paddingRight: 40, width: 0, height: 140)
+
     }
 }
